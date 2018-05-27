a21da31c7d2b37b7a5adc93930f658465bf5cf5d
Mücahit Nezir <mucahitnezir@gmail.com>
2018-05-28 00:40:14 +0300

    Firebase integration.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+	firebase: {
+    apiKey: 'AIzaSyAv0tR7c-wYYizr_sGbvrRnrvXcIQ7Fzug',
+    authDomain: 'mn-firebase-tuturials.firebaseapp.com',
+    databaseURL: 'https://mn-firebase-tuturials.firebaseio.com',
+    projectId: 'mn-firebase-tuturials',
+    storageBucket: 'mn-firebase-tuturials.appspot.com',
+    messagingSenderId: '337857243997'
+  }
 };
