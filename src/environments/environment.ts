0f14c7aeaa5e5364d82d9ae0f79d81231f554797
Mücahit Nezir <mucahitnezir@gmail.com>
2018-05-27 21:08:21 +0300

    Angular configurations.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAv0tR7c-wYYizr_sGbvrRnrvXcIQ7Fzug',
+    authDomain: 'mn-firebase-tuturials.firebaseapp.com',
+    databaseURL: 'https://mn-firebase-tuturials.firebaseio.com',
+    projectId: 'mn-firebase-tuturials',
+    storageBucket: 'mn-firebase-tuturials.appspot.com',
+    messagingSenderId: '337857243997'
+  }
 };
